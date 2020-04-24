6aa67ccf28e3566db89921c1e4a36e48d9f6b204
gir004 <tagir200917@gmail.com>
2020-04-24 15:47:22 +0300

    Code refactoring

--- js/age.js
+++ js/age.js
@@ -1,6 +1,6 @@
 function calculate_age() {
-    let dob = new Date(2002, 11, 4)
-    let diff_ms = Date.now() - dob.getTime();
+    let date_of_birth = new Date(2002, 11, 4)
+    let diff_ms = Date.now() - date_of_birth.getTime();
     let age_dt = new Date(diff_ms);
 
     return Math.abs(age_dt.getUTCFullYear() - 1970)
