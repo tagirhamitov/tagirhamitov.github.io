77852618a0e41f4c608a5e02539ac02fcab07762
Tagir Khamitov <42318996+tagirhamitov@users.noreply.github.com>
2020-11-06 16:58:08 +0300

    Fix bug in age.js

--- js/age.js
+++ js/age.js
@@ -1,5 +1,5 @@
 function calculate_age() {
-    let date_of_birth = new Date(2002, 11, 4)
+    let date_of_birth = new Date(2002, 10, 4)
     let diff_ms = Date.now() - date_of_birth.getTime();
     let age_dt = new Date(diff_ms);
 
