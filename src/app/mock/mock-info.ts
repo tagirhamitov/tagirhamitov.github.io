943e9aa579788af6ebcec988c09a736c881c881d
Tagir Khamitov <tagir200917@gmail.com>
2021-10-01 07:59:06 +0300

    Update information

--- src/app/mock/mock-info.ts
+++ src/app/mock/mock-info.ts
@@ -4,6 +4,6 @@ export const INFO: Info = {
   firstName: 'Tagir',
   lastName: 'Khamitov',
   birthDateStr: 'November 4, 2002',
-  status: 'CS student at HSE University, STEP intern at Google',
+  status: 'CS student at HSE University',
   occupation: 'Software Engineering'
 };
